1a73d3b273766eb5e9787a780fb89e6e6e8d04e4
OllieHoust <olivergrimes@houst.com>
2024-12-04 22:17:50 +0000

    feat: add Firebase Admin SDK for API authentication

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -4,5 +4,8 @@ declare namespace NodeJS {
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: string;
     STRIPE_EMBED_MONTHLY_PRICE_ID: string;
     NEXT_PUBLIC_BASE_URL: string;
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: string;
+    FIREBASE_CLIENT_EMAIL: string;
+    FIREBASE_PRIVATE_KEY: string;
   }
 } 
