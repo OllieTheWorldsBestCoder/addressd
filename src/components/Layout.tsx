eac1069eb10234b498edfd379e58727312d9e94a
OllieHoust <olivergrimes@houst.com>
2024-12-05 23:01:29 +0000

    fix: update logo display order and add favicon

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -51,18 +51,20 @@ export default function Layout({ children }: LayoutProps) {
           {/* Logo */}
           <Link href="/" className="flex items-center">
             <Image
-              src="/images/addressd-light.png"
-              alt="addressd logo"
-              width={150}
-              height={40}
-              className="hidden dark:block"
-            />
-            <Image
               src="/images/addressd-logo.png"
               alt="addressd logo"
               width={150}
               height={40}
               className="block dark:hidden"
+              priority
+            />
+            <Image
+              src="/images/addressd-light.png"
+              alt="addressd logo"
+              width={150}
+              height={40}
+              className="hidden dark:block"
+              priority
             />
           </Link>
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
 import { AppProps } from 'next/app';
 import Script from 'next/script';
 import { Analytics } from '@vercel/analytics/react';
+import Head from 'next/head';
 import '../styles/globals.css';
 import '../styles/prism.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link rel="icon" type="image/png" href="/images/addressd-logo.png" />
+        <link rel="apple-touch-icon" href="/images/addressd-logo.png" />
+      </Head>
       <Script
         src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
         strategy="lazyOnload"
